400ca81403d7f96d7ee49f2f901ae4cd6fa0e3ce
sofia <156682947+ssossofiacii@users.noreply.github.com>
2024-01-18 15:26:37 +0100

    Add files via upload

---
+++ ex35.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    char parola [3]= {':',')','\0',};
+    
+    printf ("%s\n",parola);
+    return 0;
+}
---
+++ ex36.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+    char stringa [5]={'c','i','a','o','\0'};
+    stringa [0]='C';
+    stringa [3]='O';
+    printf ("%s\n",stringa);
+
+}
