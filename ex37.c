4ff57570b35a8c722ecd6224bc870ab8782a23e3
sofia <156682947+ssossofiacii@users.noreply.github.com>
2024-01-18 16:23:35 +0100

    Add files via upload

--- ex37.c
+++ ex37.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    
+    int i=0;
+    char pippo [5];
+    scanf ("%s",pippo);
+    printf ("%s\n", pippo);
+    while (pippo[i]!='\0'){
+        if ((pippo[i])>=97 && (pippo[i])<=122){
+            pippo [i]= pippo [i]-32;
+            i= i++;
+        }
+    } 
+    printf ("%s\n",pippo);
+return 0;
 }
